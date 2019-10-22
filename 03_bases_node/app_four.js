3c1085617f1f589fdeaf5beecd80fad7d39f908b
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-10-21 23:20:29 -0300

    en el archivo ap_three se utiliza el comando listar, donde se utiliza yargs para imprimir en consola la tabla de multiplicar. Además en este ejercicio donde se utiliza el command y se detalla los objetos base y limite , vistos en consola

---
+++ 03_bases_node/app_four.js
@@ -0,0 +1,57 @@
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .command('crear', 'genera archivo con la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+
+    
+const { createFile, listarTabla } = require('../multiplicar/multiplicar')
+
+/* COMANDOS INDEPENDIENTES */
+
+//console.log(argv);
+
+//node app_four =>  { _: [], '$0': 'app_four' } 
+//node app_four crear listar borrar comer => { _: [ 'crear', 'listar', 'borrar', 'comer' ], '$0': 'app_four' }
+
+/**************************************************************************************************************** */
+let comando = argv._[0];
+switch (comando) {
+    case 'listar':
+            listarTabla(argv.base,argv.limite);
+            
+
+    case 'crear':
+
+        createFile(argv.base,argv.limite)
+            .then(archivo => {
+                console.log(`Archivo creado ${archivo}`);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        break;
+    default:
+        console.log('comando no reconocido');
+}
+
+
+
